59980f7e0a2960aefd8ae030f2e4c88be241b46d
Marko <markodjuric351@gmail.com>
2018-02-25 13:10:46 +0100

    Updating items and total calories

--- app.js
+++ app.js
@@ -55,6 +55,20 @@ const ItemCtrl = (function(){
       });
       return found;
     },
+    updatedItem: function(name, calories){
+      // Calories to number
+      calories = parseInt(calories);
+
+      let found = null;
+      data.items.forEach(item => {
+        if(item.id === data.currentItem.id){
+          item.name = name;
+          item.calories = calories;
+          found = item;
+        }
+      });
+      return found;
+    },
     setCurrentItem: function(item){
       data.currentItem = item;
     },
@@ -84,6 +98,7 @@ const ItemCtrl = (function(){
 const UICtrl = (function(){
   const UISelectors = {
     itemList: '#item-list', //ul
+    listItems: '#item-list li',
     addBtn: '.add-btn', // add meal
     updateBtn: '.update-btn', 
     deleteBtn: '.delete-btn', 
@@ -132,6 +147,21 @@ const UICtrl = (function(){
       //Insert item
       document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
     },
+    updateListItem: function(item){
+      let listItems = document.querySelectorAll(UISelectors.listItems);
+      // Turn Node list into array
+      listItems = Array.from(listItems);
+
+      listItems.forEach(listItem => {
+        const itemID = listItem.getAttribute('id');
+        if(itemID === `item-${item.id}`){
+          document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content">
+            <i class="edit-item fa fa-pencil"></i>
+          </a>`;
+        }
+      });
+    },
     clearInput: function(){
       document.querySelector(UISelectors.itemNameInput).value = '';
       document.querySelector(UISelectors.itemCaloriesInput).value = '';
@@ -155,7 +185,6 @@ const UICtrl = (function(){
       document.querySelector(UISelectors.addBtn).style.display = 'inline';
     },
     showEditState: function(){
-      // UICtrl.clearInput();
       document.querySelector(UISelectors.updateBtn).style.display = 'inline';
       document.querySelector(UISelectors.deleteBtn).style.display = 'inline';
       document.querySelector(UISelectors.backBtn).style.display = 'inline';
@@ -178,8 +207,17 @@ const AppCtrl = (function(ItemCtrl, UICtrl){
     const UISelectors = UICtrl.getSelectors();
     //Add item event
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    // Disable submit on enter
+    document.addEventListener('keypress', function(e){
+      if(e.keyCode === 13 || e.which === 13){
+        e.preventDefault();
+        return false;
+      }
+    });
     // Edit icon click
-    document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+    document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
+    // Update item event
+    document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);    
   }
 
   // Add item Submit
@@ -203,8 +241,8 @@ const AppCtrl = (function(ItemCtrl, UICtrl){
     e.preventDefault();
   }
 
-  // Update Item submit
-  const itemUpdateSubmit = function(e){
+  // Click edit item
+  const itemEditClick = function(e){
     if(e.target.classList.contains('edit-item')){
       // Get list item ID
       const listId = e.target.parentNode.parentNode.id;
@@ -220,6 +258,25 @@ const AppCtrl = (function(ItemCtrl, UICtrl){
       // Add item to form
       UICtrl.addItemToForm();
     }
+
+    e.preventDefault();
+  }
+
+  // Item update submit
+  const itemUpdateSubmit = function(e){
+    // get item input
+    const input = UICtrl.getItemInput();
+    //Update item
+    const updatedItem = ItemCtrl.updatedItem(input.name, input.calories);
+    // Update UI
+    UICtrl.updateListItem(updatedItem);
+
+     // get total calories
+     const totalCalories = ItemCtrl.getTotalCalories();
+     // Add total calories to UI
+     UICtrl.showTotalCalories(totalCalories);
+
+     UICtrl.clearEditState();
 
     e.preventDefault();
   }
